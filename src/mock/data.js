5f491da30cd09c4a723e30c1fea99e2fa66e1485
taty500summers <tatiana.bernon@gmail.com>
2020-10-28 16:29:37 +1300

    Modify mock heroData

--- src/mock/data.js
+++ src/mock/data.js
@@ -9,10 +9,10 @@ export const headData = {
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  title: '', // Hello, my name is
+  name: '', // Tatiana
+  subtitle: '', // I'm the Unknown Developer
+  cta: '', // Know more
 };
 
 // ABOUT DATA
